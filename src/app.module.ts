9d2b541dd1f6fe44c8d4d4babe2177f28efe789c
“Woo TaeHyeon <wth2052@gmail.com>
2023-02-15 12:41:48 +0900

    😎FEAT: CRUD base structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
+import { ApiController } from './api/api.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [UsersModule],
+  controllers: [AppController, UsersController, ApiController],
   providers: [AppService],
 })
 export class AppModule {}
